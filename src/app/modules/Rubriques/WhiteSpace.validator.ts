bf884da25d373a97064d969844d407e0130e79a7
ghliel <hatimghliel00@gmail.com>
2020-03-26 22:51:27 +0100

    whitespace bug fixed

--- src/app/modules/Rubriques/WhiteSpace.validator.ts
+++ src/app/modules/Rubriques/WhiteSpace.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidationErrors } from '@angular/forms';
   
 export class WhiteSpaceValidator {
     static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
-        if((control.value as string).indexOf(' ') >= 0){
+        if((control.value as string).startsWith(' ') || (control.value as string).endsWith(' ') ){
             return {cannotContainSpace: true}
         }
   
